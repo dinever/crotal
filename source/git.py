00e072a77033f5dd45b9a9b6d1b7377566a9cc66
dinever <dinever@live.cn>
2013-11-13 17:51:37 +0800

    Adding git function

--- source/git.py
+++ source/git.py
@@ -15,9 +15,44 @@ def rsyncDeploy(dir):
     os.system("git add .")
     os.system("git add -u")
     message = "Site updated at %s" % datetime.strptime(datetime.now(), "%Y-%m-%d %H:%M:%S")
-    os.system("git commit -m %s" % message)
+    os.system("git commit -m '%s'" % message)
     os.system("git push origin %s" % deploy_branch --force)
     print "Github Pages deploy complete"
 
+def setupGithubPages():
+    print "Enter the read/write url for your repository"
+    print "(For example, 'git@github.com:your_username/your_username.github.io)"
+    repo_url = raw_inpout("Repository url: ")
+
 if __name__ == '__main__':
     rsyncDeploy(dir)
+
+#  if args.repo
+#    repo_url = args.repo
+#  else
+#    puts "Enter the read/write url for your repository"
+#    puts "(For example, 'git@github.com:your_username/your_username.github.io)"
+#    repo_url = get_stdin("Repository url: ")
+#  end
+#  user = repo_url.match(/:([^\/]+)/)[1]
+#  branch = (repo_url.match(/\/[\w-]+\.github\.(?:io|com)/).nil?) ? 'gh-pages' : 'master'
+#  project = (branch == 'gh-pages') ? repo_url.match(/\/([^\.]+)/)[1] : ''
+#  unless (`git remote -v` =~ /origin.+?octopress(?:\.git)?/).nil?
+#    # If octopress is still the origin remote (from cloning) rename it to octopress
+#    system "git remote rename origin octopress"
+#    if branch == 'master'
+#      # If this is a user/organization pages repository, add the correct origin remote
+#      # and checkout the source branch for committing changes to the blog source.
+#      system "git remote add origin #{repo_url}"
+#      puts "Added remote #{repo_url} as origin"
+#      system "git config branch.master.remote origin"
+#      puts "Set origin as default remote"
+#      system "git branch -m master source"
+#      puts "Master branch renamed to 'source' for committing your blog source files"
+#    else
+#      unless !public_dir.match("#{project}").nil?
+#        system "rake set_root_dir[#{project}]"
+#      end
+#    end
+#  end
+#
