af5f7c8b5abd5b5925714dc0f40ff04512ef0d02
dinever <dinever@live.cn>
2014-01-11 01:48:25 +0800

    upadte

--- crotal/config.py
+++ crotal/config.py
@@ -5,8 +5,8 @@ class Config():
     '''
     Read config from _config.yml
     '''
-    def __init__(self):
-        self.dir = Path(__file__).ancestor(2).absolute()
+    def __init__(self, dir):
+        self.dir = dir
         self.config_yml = open(self.dir + '/_config.yml', 'r').read()
         self.config = yaml.load(self.config_yml)
         for item in self.config:
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os, sys, timeit
 from unipath import Path
 
-import settings
-from turtpress.views import Views
-from turtpress.copydir import copy_dir
+import config
+from crotal.views import Views
+from crotal.copydir import copy_dir
+from crotal.config import Config
 
 dir = Path(__file__).ancestor(1).absolute()
+config = Config(dir)
 
 def remove_site():
     '''
@@ -13,18 +15,22 @@ def remove_site():
     '''
     import shutil
     try:
-        shutil.rmtree(dir + '/' + '_sites/')
+        shutil.rmtree(dir + '/_sites/')
     except:
         pass
 
 def generate_site():
+
+    #Read config file
+    print config.theme
+
     remove_site()
     start = timeit.default_timer()
-    copy_dir('themes/' + settings.theme + '/static','_sites')
+    copy_dir('themes/' + config.theme + '/static','_sites')
     copydir_time = timeit.default_timer()
     print '{0:20} in {1:3.3f} seconds'.format('Static Files Copied', copydir_time - start)
 
-    view = Views()
+    view = Views(config)
     view.get_directory(dir)
     view.get_posts()
     get_posts_time = timeit.default_timer()
@@ -59,26 +65,29 @@ if __name__ == '__main__':
             generate_site()
         elif sys.argv[1] == 'server':
             del sys.argv[1]
-            from turtpress import server
+            from crotal import server
             server.dir = dir
             server.main()
         elif sys.argv[1] == 'new_page':
             del sys.argv[1]
-            from turtpress import create_pages
-            create_pages.create_page()
+            from crotal import create_pages
+            create_pages.create_page(config)
         elif sys.argv[1] == 'new_post':
             del sys.argv[1]
-            from turtpress import create_post
+            from crotal import create_post
             create_post.create_post()
         elif sys.argv[1] == 'deploy':
-            if settings.deploy_default == 'rsync':
-                from turtpress import rsync
-                rsync.rsyncDeploy(str(dir) + '/_sites/')
-            elif settings.deploy_default == 'git':
-                from turtpress import git
-                git.rsyncDeploy()
+            if config.deploy_default == 'rsync':
+                from crotal import rsync
+                rsync.rsync_deploy(str(dir) + '/_sites/', config)
+            elif config.deploy_default == 'git':
+                from crotal import git
+                git.rsync_deploy()
             else:
                 print 'Only support rsync for now.'
+        elif sys.argv[1] == 'init':
+            ##TODO
+            pass
         else:
             usage()
     else:
