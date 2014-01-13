a655de7a0f7554062d2339598e2fd01663d2432e
dinever <dinever@live.cn>
2014-01-13 09:11:08 +0800

    font buf fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "crotal",
-    version = "0.2.2",
+    version = "0.2.3",
     packages = find_packages(),
 
     include_package_data = True,
@@ -20,9 +20,9 @@ setup(
     package_data = {
         'crotal' : [ '*.data' ]
     },
-    install_requires = ['Jinja2==2.7.1','markdown==2.3.1','unipath==1.0','PyYAML==3.10'],
+    install_requires = ['Jinja2==2.7.1','markdown==2.3.1','PyYAML==3.10'],
     author = "Dinever",
     author_email = 'dingpeixuan911@gmail.com',
-    url = "http://www.dinever.com",
+    url = "http://github.com/dinever/crotal",
     description = 'A static site generator written in python.',
 )
