38289a8f0ba1ae75932dc2cd0bc7bb7077f5f77f
dinever <dinever@live.cn>
2014-01-19 20:09:16 +0800

    update create_post.py

--- crotal/create_post.py
+++ crotal/create_post.py
@@ -17,26 +17,29 @@ slug: %s
 
 """
 
+def usage():
+    print 'Usage:'
+    print 'crotal new_post "you title here"'
+    print '#You post will be created in _posts/'
+
 def create_post(config):
     flag = 0
     if len(sys.argv) == 1:
-        print 'Usage: deploy'
-
+        usage()
     elif len(sys.argv) != 1:
         post = Posts(config)
-        test = PinYin()
-        test.load_word()
-        string = sys.argv[1]
-        dt = datetime.now()
+        pinyin = PinYin()
+        pinyin.load_word()
+        string = sys.argv[1] #Assume that argv[1] is the title user inputed.
         post.title = string
-        post.slug = test.hanzi2pinyin_split(string=string, split="-")
+        post.slug = pinyin.hanzi2pinyin_split(string=string, split="-")
+        dt = datetime.now()
         post.pub_time = dt.strftime("%Y-%m-%d %H:%M")
         new_post = SAMPLE % (post.title, post.pub_time, post.slug)
         file_title = dt.strftime("%Y-%m-%d") + '-' + post.slug + '.markdown'
         open('_posts/' + file_title, 'w+').write(new_post)
     else:
-        usraccount = sys.argv[1]
-        passwd = sys.argv[2]
+        usage()
 
 if __name__ == '__main__':
     create_post()
