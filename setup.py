a35598a8cb0b60d6468a40504a65c203bb7bc6db
dinever <dinever@live.cn>
2014-01-24 00:22:26 +0800

    upgrade to 0.3.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "crotal",
-    version = "0.3.2",
+    version = "0.3.3",
     packages = find_packages(),
 
     include_package_data = True,
