4126d5fe05fbabc313be8873295cd0ee42dcc009
dinever <dinever@live.cn>
2014-01-23 12:12:37 +0800

    Updating to version 0.3.0 with brand new theme

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "crotal",
-    version = "0.2.4",
+    version = "0.3.0",
     packages = find_packages(),
 
     include_package_data = True,
