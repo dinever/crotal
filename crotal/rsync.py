b9ac1643ef93faf40f6bd1e7eef646907f51f5e0
lb1a <github@lb1a.de>
2014-04-30 14:47:37 +0200

    Adding support for local rsync deployment

--- crotal/rsync.py
+++ crotal/rsync.py
@@ -2,8 +2,13 @@ import os
 
 
 def rsync_deploy(dir, config):
-    print 'Deploying by Rsync ...'
-    os.system("rsync -avz %s %s:%s" % (dir, config.ip, config.deploy_dir))
+
+    if config.ip and config.ip is not None:
+        print 'Deploying by Rsync to %s:%s ...' % (config.ip, config.deploy_dir)
+        os.system("rsync -avz %s %s:%s" % (dir, config.ip, config.deploy_dir))
+    else:
+        print 'Deploying by Rsync to %s ...' % config.deploy_dir
+        os.system("rsync -avz %s %s" % (dir, config.deploy_dir))
 
 if __name__ == '__main__':
     rsync_deploy(dir)
