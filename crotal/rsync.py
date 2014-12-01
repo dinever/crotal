efc0e207feb11a98d5215421d35803db229d1968
丁培轩 <dinever@dingpeixuans-MacBook-Pro.local>
2014-12-01 15:48:14 +0800

    Removing irrelative files

--- crotal/rsync.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from crotal.config import config
-
-def rsync_deploy():
-    print 'Deploying by Rsync ...'
-    print config.deploy_dir
-    os.system("rsync -avz %s %s:%s" % (config.publish_dir, config.ip, config.deploy_dir))
